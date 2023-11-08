5ef3c7a64f11dc204aa18ff2dcc478181566f60c
halimatou-cpu <halimatoubah.bh@gmail.com>
2023-11-08 18:00:20 +0100

    pragma & compilers version struggles fixed

--- contracts/DiplomaToken.sol
+++ contracts/DiplomaToken.sol
@@ -1,120 +1,119 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.21;
+pragma solidity >=0.4.25 <0.9.0;
 
-// import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-// import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract DiplomaToken {
+contract DiplomaToken is ERC721 {
     uint256 private _nextTokenId;
 
-    constructor()  {}
+    constructor() ERC721("DiplomaToken", "DTK") {}
 
-    // struct Diploma {
-    //     string name;
-    //     address school;
-    //     address student;
-    //     string diplomaHash;
-    //     uint256 date;
-    // }
+    struct Diploma {
+        string name;
+        address school;
+        address student;
+        string diplomaHash;
+        uint256 date;
+    }
 
-    // mapping(address => string) public schoolNames;
-    // address[] public schools;
-    // Diploma[] public diplomas;
-    // event DiplomaMinted(
-    //     uint256 tokenId,
-    //     string name,
-    //     address school,
-    //     address student,
-    //     string diplomaHash,
-    //     uint256 date
-    // );
-    // event SchoolCreated(string name, address school);
+    mapping(address => string) public schoolNames;
+    address[] public schools;
+    Diploma[] public diplomas;
+    event DiplomaMinted(
+        uint256 tokenId,
+        string name,
+        address school,
+        address student,
+        string diplomaHash,
+        uint256 date
+    );
+    event SchoolCreated(string name, address school);
 
-    // // function createSchool(string memory _name) public {
-    // //     schools.push(msg.sender);
-    // //     schoolNames[msg.sender] = _name;
-    // //     emit SchoolCreated(_name, msg.sender);
-    // // }
+    function createSchool(string memory _name) public {
+        schools.push(msg.sender);
+        schoolNames[msg.sender] = _name;
+        emit SchoolCreated(_name, msg.sender);
+    }
 
-    // function getSchoolName(
-    //     address schoolAddress
-    // ) public view returns (string memory) {
-    //     return schoolNames[schoolAddress];
-    // }
+    function getSchoolName(
+        address schoolAddress
+    ) public view returns (string memory) {
+        return schoolNames[schoolAddress];
+    }
 
-    // modifier onlySchool() {
-    //     bool isSchool = false;
-    //     for (uint256 i = 0; i < schools.length; i++) {
-    //         if (schools[i] == msg.sender) {
-    //             isSchool = true;
-    //             break;
-    //         }
-    //     }
-    //     require(isSchool, "Only school can call this function");
-    //     _;
-    // }
+    modifier onlySchool() {
+        bool isSchool = false;
+        for (uint256 i = 0; i < schools.length; i++) {
+            if (schools[i] == msg.sender) {
+                isSchool = true;
+                break;
+            }
+        }
+        require(isSchool, "Only school can call this function");
+        _;
+    }
 
-    // function createDiploma(
-    //     string memory _name,
-    //     address _student,
-    //     string memory _hash
-    // ) public onlySchool {
-    //     // _safeMint(_student, _nextTokenId);
-    //     _nextTokenId++;
-    //     diplomas.push(
-    //         Diploma({
-    //             name: _name,
-    //             school: msg.sender,
-    //             student: _student,
-    //             diplomaHash: _hash,
-    //             date: block.timestamp
-    //         })
-    //     );
-    //     emit DiplomaMinted(
-    //         _nextTokenId,
-    //         _name,
-    //         msg.sender,
-    //         _student,
-    //         _hash,
-    //         block.timestamp
-    //     );
-    // }
+    function createDiploma(
+        string memory _name,
+        address _student,
+        string memory _hash
+    ) public onlySchool {
+        _safeMint(_student, _nextTokenId);
+        _nextTokenId++;
+        diplomas.push(
+            Diploma({
+                name: _name,
+                school: msg.sender,
+                student: _student,
+                diplomaHash: _hash,
+                date: block.timestamp
+            })
+        );
+        emit DiplomaMinted(
+            _nextTokenId,
+            _name,
+            msg.sender,
+            _student,
+            _hash,
+            block.timestamp
+        );
+    }
 
-    // function getStudentDiplomas(
-    //     address _student
-    // ) public view returns (Diploma[] memory) {
-    //     Diploma[] memory myDiplomas;
-    //     for (uint256 i = 0; i < diplomas.length; i++) {
-    //         if (diplomas[i].student == _student) {
-    //             myDiplomas[i] = diplomas[i];
-    //         }
-    //     }
-    //     return myDiplomas;
-    // }
+    function getStudentDiplomas(
+        address _student
+    ) public view returns (Diploma[] memory) {
+        Diploma[] memory myDiplomas;
+        for (uint256 i = 0; i < diplomas.length; i++) {
+            if (diplomas[i].student == _student) {
+                myDiplomas[i] = diplomas[i];
+            }
+        }
+        return myDiplomas;
+    }
 
-    // function getSchools() public view returns (address[] memory) {
-    //     return schools;
-    // }
+    function getSchools() public view returns (address[] memory) {
+        return schools;
+    }
 
-    // function getSchoolDiplomas(
-    //     address _school
-    // ) public view returns (Diploma[] memory) {
-    //     Diploma[] memory myDiplomas;
-    //     for (uint256 i = 0; i < diplomas.length; i++) {
-    //         if (diplomas[i].school == _school) {
-    //             myDiplomas[i] = diplomas[i];
-    //         }
-    //     }
-    //     return myDiplomas;
-    // }
+    function getSchoolDiplomas(
+        address _school
+    ) public view returns (Diploma[] memory) {
+        Diploma[] memory myDiplomas;
+        for (uint256 i = 0; i < diplomas.length; i++) {
+            if (diplomas[i].school == _school) {
+                myDiplomas[i] = diplomas[i];
+            }
+        }
+        return myDiplomas;
+    }
 
-    // // function _safeTransfer(
-    // //     address from,
-    // //     address to,
-    // //     uint256 tokenId
-    // // ) internal override(ERC721) {
-    // //     require(false, "Can't transfer this token's ownership");
-    // //     super._safeTransfer(from, to, tokenId, "");
-    // // }
+    function _transfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override {
+        require(false, "Can't transfer this token's ownership");
+        super._transfer(from, to, tokenId);
+    }
 
 }
--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,11 +1,5 @@
-// const DiplomaToken = artifacts.require("DiplomaToken");
-
-// module.exports = function(deployer) {
-// 	deployer.deploy(DiplomaToken);
-// };
-const Test = artifacts.require("Test");
+const DiplomaToken = artifacts.require("DiplomaToken");
 
 module.exports = function(deployer) {
-	deployer.deploy(Test);
+	deployer.deploy(DiplomaToken);
 };
-
