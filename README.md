ba9b3e026b4fa430745a109e695d36a48e00240e
halimatou-cpu <halimatoubah.bh@gmail.com>
2024-01-05 23:12:41 +0100

    deployment config & Readme updated with project's description and steps

--- README.md
+++ README.md
@@ -1,1 +1,81 @@
 # alumni-smart-contract
+
+Based on the [ERC-721] standard, this smart contract allows the creation of a non-fungible token (NFT) that represents a student's degree. The NFT is minted by the university and sent to the student's wallet. The student can then transfer the NFT to a potential employer, who can verify the student's degree by checking the NFT's metadata.
+
+### Prerequisites
+
+- [Node.js](https://nodejs.org/en/)
+- [Truffle](https://www.trufflesuite.com/truffle)
+- [Ganache](https://www.trufflesuite.com/ganache)
+- [Metamask](https://metamask.io/)
+- [OpenZeppelin](https://openzeppelin.com/)
+- [Infura](https://infura.io/)
+
+### Installation
+
+1. Clone the repo
+   ```sh
+   git clone
+   ```
+2. Install NPM packages
+   ```sh
+   npm install
+   ```
+3. Copy the .env.example file and rename it to .env
+   ```sh
+   cp .env.example .env
+   ```
+4. Fill in the .env file with your Infura project ID and your Metamask mnemonic
+   ```sh
+   INFURA_PROJECT_ID=your_infura_project_id
+   MNEMONIC=your_metamask_mnemonic
+   ```
+5. Compile the smart contract
+   ```sh
+   $truffle compile or $npm run compile
+   ```
+6. Start Ganache
+	 ```sh
+	 $ganache
+	 ```
+7. Migrate the smart contract to the blockchain
+   ```sh
+   $truffle migrate or $npm run migrate
+   ```
+8. Run the tests
+   ```sh
+   $truffle test or $npm run test
+   ```
+
+### Deployment to Sepolia testnet
+
+1. Create a new project on [Infura](https://infura.io/)
+2. Copy the project ID and paste it in the .env file
+	 ```sh
+	 INFURA_PROJECT_ID=your_infura_project_id
+	 ```
+3. Create a new wallet on [Metamask](https://metamask.io/)
+4. Copy the mnemonic and paste it in the .env file
+	 ```sh
+	 MNEMONIC=your_metamask_mnemonic
+	 ```
+5. Enable the Sepolia testnet network in truffle-config.js
+6. Run 
+	 ```sh
+	 truffle deploy --network sepolia
+	 ```
+
+
+<!-- USAGE EXAMPLES -->
+## Usage
+
+### Interacting with the smart contract on the Sepolia testnet
+
+In your terminal, run the following command to start the truffle console:
+```sh
+$truffle console --network sepolia
+```
+In the truffle console, run the following commands to interact with the smart contract:
+```sh
+truffle(sepolia)> let instance = await DiplomaToken.deployed()
+```
--- truffle-config.js
+++ truffle-config.js
@@ -41,10 +41,11 @@
  * https://trufflesuite.com/docs/truffle/getting-started/using-the-truffle-dashboard/
  */
 
-// require('dotenv').config();
-// const { MNEMONIC, PROJECT_ID } = process.env;
+require("dotenv").config();
+const { MNEMONIC, PROJECT_ID } = process.env;
+// const { API_URL, PRIVATE_KEY } = process.env;
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 
 module.exports = {
   /**
@@ -65,9 +66,9 @@ module.exports = {
     // options below to some value.
     //
     development: {
-     host: "127.0.0.1",     // Localhost (default: none)
-     port: 8545,            // Standard Ethereum port (default: none)
-     network_id: "*",       // Any network (default: none)
+      host: "127.0.0.1", // Localhost (default: none)
+      port: 8545, // Standard Ethereum port (default: none)
+      network_id: "*", // Any network (default: none)
     },
     //
     // An additional network, but with some advanced options…
@@ -89,6 +90,27 @@ module.exports = {
     //   timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
     //   skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     // },
+
+    // goerli: {
+    //   provider: () => new HDWalletProvider(MNEMONIC, `https://goerli.infura.io/v3/${PROJECT_ID}`),
+    //   network_id: 5,       // Goerli's id
+    //   confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
+    //   timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+    //   skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    // },
+
+    sepolia: {
+      provider: () =>
+        new HDWalletProvider(
+          MNEMONIC,
+          `https://sepolia.infura.io/v3/${PROJECT_ID}`
+        ),
+      network_id: 11155111, // Sepolia's id
+      confirmations: 2, // # of confirmations to wait between deployments. (default: 0)
+      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+    },
+
     //
     // Useful for private networks
     // private: {
@@ -106,7 +128,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.17" // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.8.17", // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
@@ -115,8 +137,8 @@ module.exports = {
       //  },
       //  evmVersion: "byzantium"
       // }
-    }
-  }
+    },
+  },
 
   // Truffle DB is currently disabled by default; to enable it, change enabled:
   // false to enabled: true. The default storage location can also be
